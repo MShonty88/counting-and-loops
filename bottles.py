a6b8e3bc520cffc0c71e5103651e02e2d12fa4ab
Mike Shonty <shonty@wisc.edu>
2024-02-09 18:07:23 -0600

    Completed up through exercise 6.

--- bottles.py
+++ bottles.py
@@ -4,3 +4,8 @@ for i in range (100, 0, -1):
         print(i, "bottles of beer")
         print("Take one down, pass it around")
         print(i, "bottles of beer on the wall")
+        if i == 5:
+                print("If one of those bottles should happen to fall, 5 bottles of beer on the wall...")
+                print("")
+print("")
+print("No more bottles of beer on the wall, no more bottles of beer.\nWe've taken them down and passed them around; now we're drunk and passed out!")
